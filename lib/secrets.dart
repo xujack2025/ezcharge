8499414966c1c38f018daec12586ffc7e73ea585
xujack <xujack2003@gmail.com>
2025-09-18 21:32:47 +0800

    import old code

---
+++ lib/secrets.dart
@@ -0,0 +1,5 @@
+class Secrets {
+  static const String googleMapsApiKey = "YOUR_GOOGLE_MAPS_API_KEY_HERE";
+}
+
+const OPENAI_API_KEY = "YOUR_OPENAI_API_KEY_HERE";
---
+++ lib/views/customer/Service/api_key.dart
@@ -0,0 +1,3 @@
+class APIKey {
+  static const apiKey = "YOUR_OPENAI_API_KEY_HERE";
+}
