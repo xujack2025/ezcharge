59a42facfb633d077f7ad58b2046b957c5812dbd
xujack2025 <233577584+xujack2025@users.noreply.github.com>
2025-12-10 12:54:40 +0800

    Remove exposed API keys

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,5 +1,7 @@
 class Secrets {
+  // TODO: Add your Google Maps API key from environment or config
   static const String googleMapsApiKey = "YOUR_GOOGLE_MAPS_API_KEY_HERE";
 }
 
+// TODO: Add your OpenAI API key from environment or config
 const OPENAI_API_KEY = "YOUR_OPENAI_API_KEY_HERE";
--- lib/views/customer/Service/api_key.dart
+++ lib/views/customer/Service/api_key.dart
@@ -1,3 +1,4 @@
 class APIKey {
+  // TODO: Add your OpenAI API key from environment or config
   static const apiKey = "YOUR_OPENAI_API_KEY_HERE";
 }
